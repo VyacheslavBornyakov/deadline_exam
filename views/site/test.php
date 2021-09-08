4ae468acace66d0ac147e4ca50d7f12b01288d6e
jimmm03 <jimmm03@mail.ru>
2021-09-08 23:16:14 +0300

    first commit

---
+++ views/site/test.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+
+<form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">для ввода названия экзамена, латинские буквы, не более 10 символов.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">выбор даты, день когда состояться экзамен, .</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">количество дней ti для подготовки к данному экзамену, после которых Василий все забудет  .</div>
+    </div>
+    <button type="submit" class="btn btn-primary">Отправить</button>
+</form>
+
